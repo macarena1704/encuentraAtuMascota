b414fad3841acb8a0fb7b7c1eae812c7865e5867
macarena1704 <macarenafloccoreitano@hotmail.com>
2023-09-13 20:34:59 -0300

    agregue function + if/else + alert

---
+++ script.js
@@ -0,0 +1,14 @@
+function iniciarSesion() {
+    let inicioSesion = prompt ("¿Desea iniciar sesión? Indique SI para iniciar o NO para continuar sin iniciar");
+        if (inicioSesion === "SI") {
+            let nombre = prompt ("Ingrese su nombre");
+            let password = prompt ("Ingrese su clave");
+            alert ("Bienvenido/a " + nombre + " a Encuentra a Tu Mascota")
+        } else if (inicioSesion === "NO") {
+            alert("Muchas gracias! Puede continuar navegando sin iniciar sesión");
+        } else {
+            alert("Ha ingresado una opción inválida. Indique SI para iniciar sesión o NO para continuar sin iniciar ");
+        }
+}
+
+iniciarSesion ()
